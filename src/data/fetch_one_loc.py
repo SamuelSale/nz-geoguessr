77b394040fc851490b5402cca0e8a69eb79cab8a
Samuel Sale <ssale06@gmail.com>
2026-06-24 22:38:36 +1200

    Rewrite fetch_one; fix gitignore to not ignore src/data

--- src/data/fetch_one_loc.py
+++ src/data/fetch_one_loc.py
@@ -9,7 +9,7 @@ key = os.getenv("GOOGLE_STATIC_MAPS_API")
 # 2. The Street View Static API endpoint
 url = "https://maps.googleapis.com/maps/api/streetview"
 
-# 3. Request parameters
+# 3. Request parameters (Params for EXACT LOCATION for a test)
 params = {
     "size": "640x640",                  # max resolution Google allows
     "pano": "Nr0aTB8h4Dg3Q3Kf8RZB5w",   # exact panorama: Barrytown, West Coast
@@ -18,14 +18,14 @@ params = {
 }
 
 # 4. Send the request
-resp = requests.get(url, params=params)
-print("Status:", resp.status_code)
-print("Content-Type:", resp.headers.get("Content-Type"))
-print("Bytes:", len(resp.content))
+response = requests.get(url, params=params)
+
+print("Status", response.status_code)
+print("Content-Type:", response.headers.get("Content-Type"))
+print("Bytes:", len(response.content))
+
 
 # 5. Save the image bytes to disk
 os.makedirs("data/raw", exist_ok=True)
-with open("data/raw/test.jpg", "wb") as f:
-    f.write(resp.content)
-
-print("Saved -> data/raw/test.jpg")
+with open("data/raw/testing.jgp", "wb") as f:
+    f.write(response.content)
